7b82f41c9e1d563f4b435f1a6c08637cea154527
Logan-Ward <ward_logan@outlook.com>
2022-12-03 16:50:57 -0600

    Initializing Repository

---
+++ JobHunt.py
@@ -0,0 +1,38 @@
+try:
+    import os
+    from googlesearch import search
+    from datetime import date
+    from dateutil.relativedelta import relativedelta
+    from webbrowser import get
+    from time import sleep
+    from TestOpen import testArray
+    from dotenv import load_dotenv
+    
+except ImportError:
+    print("Missing module, make sure you ran the install command")
+
+# Makes the .env variables accessible
+load_dotenv()
+
+# The date three months ago, change the 3 to any number to change how many 
+# months ago you want to get search results from
+fromDate = date.today() + relativedelta(months=-3)
+
+# Adding the date to the query
+query = '{} after:{}'.format(os.getenv('QUERY'), fromDate)
+
+# Not sure why this is necessary, but the browser path won't work without 
+# that %s in the string
+browserPath = '{} %s'.format(os.getenv('BROWSERPATH'))
+
+# Searches google then opens the first 20 results with a 3 second pause 
+# in-between each query to prevent google blocking your IP
+for j in search(query, tld="co.in", num=20, stop=20, pause=3):
+    print(j)
+    get(browserPath).open(j, new=2)
+
+# Test Browser Open
+# for j in testArray:
+#     print(j)
+#     get(browserPath).open(j, new=2)
+#     sleep(2)
---
+++ TestOpen.py
@@ -0,0 +1,6 @@
+testArray = ['https://boards.greenhouse.io/runzero/jobs/4625934004',
+'https://boards.greenhouse.io/assemblyhq/jobs/5373607003',
+'https://jobs.lever.co/apt-deco/12e8f180-85fe-44c0-b42d-b8e97d70dba3',
+'https://jobs.lever.co/akasa.com/b6c482fb-d9ce-4949-a5e5-c40352b4af44',
+'https://boards.greenhouse.io/fourkites/jobs/4492026',
+'https://jobs.lever.co/ecocart/43549346-09f0-49bd-8632-a4fabf1d3428']
